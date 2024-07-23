90dff77c0dccdcc7928771dbaad9366f48d1de8a
WTS25kt <ishii25kenta@gmail.com>
2024-07-23 13:44:39 +0900

    ポップアップをすべて許可にしたら成功
    しかし、PCが異常に熱くなる事象が発生したため、原因調査が必要

--- url_opener/script.js
+++ url_opener/script.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('openUrlButton').addEventListener('click', () => {
-      const urlTextarea = document.getElementById('urlTextarea');
-      const urls = urlTextarea.value.trim().split('\n').map(url => url.trim());
-  
-      urls.forEach(url => {
-        if (url) {
-          // URLがhttpまたはhttpsで始まっていない場合、httpを追加
-          const formattedUrl = url.startsWith('http://') || url.startsWith('https://') ? url : `http://${url}`;
-          window.open(formattedUrl, '_blank');
-        }
-      });
+  document.getElementById('openUrlButton').addEventListener('click', () => {
+    const urlTextarea = document.getElementById('urlTextarea');
+    const urls = urlTextarea.value.trim().split('\n').map(url => url.trim());
+
+    urls.forEach(url => {
+      if (url) {
+        // URLがhttpまたはhttpsで始まっていない場合、httpを追加
+        const formattedUrl = url.startsWith('http://') || url.startsWith('https://') ? url : `http://${url}`;
+        // 新しいタブでURLを開く
+        const a = document.createElement('a');
+        a.href = formattedUrl;
+        a.target = '_blank';
+        a.click();
+      }
     });
   });
+});
